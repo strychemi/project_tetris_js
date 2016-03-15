1089edbcc47cd140dec18e09d1a16db564e29ae8
Andrew Baik <and.baik@gmail.com>
2016-03-15 14:04:43 -0400

    prototype inheritance

--- model.js
+++ model.js
@@ -1,22 +1,28 @@
-var block = {
+var block = block || {};
 
-  boardBlocks: [],
+block.boardBlocks = [];
 
-  BlockConstructor: function(x, y, shape, rotation){
-    this.coord = {
-      x: x,
-      y: y,
-    };
-    this.blocks = []
-    this.pivot = 0;
-    this.y = y;
-    this.shape = shape;
-    this.rotation = rotation;
-    this.placed = false;
-  },
-
-  L
+block.BlockConstructor = function(x, y) {
+  this.coord = {
+    x: x,
+    y: y,
+  };
+  this.blocks = [];
+  this.pivot = 0;
+  this.rotation = function() {};
+  this.size = config.size;
+  this.placed = false;
 };
+
+block.LShapeConstructor = function(rotation) {
+  block.BlockConstructor.call(this, 0, 0);
+  //this.rotation = rotation;
+  this.prototype = block.BlockConstructor;
+};
+
+block.LShapeConstructor.prototype = Object.create(block.BlockConstructor.prototype);
+block.LShapeConstructor.prototype.constructor = block.LShapeConstructor;
+
 
 var board = {
 
